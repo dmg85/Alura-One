c8d83abcd09d86a5625eba179172258af0796809
Diego Medina <damgz191@gmail.com>
2022-12-03 16:48:54 -0500

    declarging variables

---
+++ script.js
@@ -0,0 +1,13 @@
+// declare variables
+
+let year = 2022;
+
+let ageJuan = ("Juan has: " + (year - 2000) + " years old");
+let ageSony = ("Sony has: " + (year - 1984) + " years old");
+let ageHelen = ("Helen has: " + (year - 1990) + " years old");
+
+
+console.log(ageJuan);
+console.log(ageSony);
+console.log(ageHelen);
+
