ff8228892add1f3bbdcb7a8e10a98ea1f9954023
Diego Medina <damgz191@gmail.com>
2022-12-06 06:00:04 -0500

    Gasoline challenge 3

---
+++ gasoline_challenge/alcohol_or_gasoline.js
@@ -0,0 +1,8 @@
+let gasoline = 480 / 40;
+
+console.log("The car makes "+ gasoline + " gallons per kilometer");
+
+let alhocol = 300 / 40;
+
+console.log("The car makes "+ alhocol + " gallons per kilometer");
+
