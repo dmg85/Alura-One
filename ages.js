5560d43698551fc8590ed28a64c1a15169a9d68f
Diego Medina <damgz191@gmail.com>
2022-12-08 14:31:37 -0500

    ages 'functions challenge'

---
+++ ages.js
@@ -0,0 +1,15 @@
+// require function to include the mdn prompt module
+const prompt = require("prompt-sync")();
+
+// Calculate the age difference with your sister
+
+
+let brother1 = prompt("What's your age = ");
+
+let brother2 = prompt("Whats my age = ");
+
+let agesSum = (ages) => console.log("The difference of age between brothers is " + (brother1 - brother2) + " year");  
+
+
+agesSum();
+
